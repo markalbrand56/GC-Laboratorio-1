f4a89d8e22a90cffbdf189d1020eae485148b56b
Mark Albrand <mark.albrand5@gmail.com>
2023-07-08 01:04:05 -0600

    Poligono

--- main.cpp
+++ main.cpp
@@ -2,72 +2,86 @@
 
 void render_point();
 void render_line(Vertex2 start, Vertex2 end);
+void render_polygon(Vertex2* points, int numPoints);
 
 int main() {
+    // Colores
+    Color navyBlue(0, 0, 128);
+    Color white(255, 255, 255);
+
+    // Color de fondo y limpiar el buffer
+    setClearColor(navyBlue);
+    clear();
+
+    // Color de dibujo
+    setCurrentColor(white);
+
     // Dibujar un solo punto
     render_point();
+    renderBuffer("point.bmp");
+    clear();
 
     // Dibujar una línea
     Vertex2 lineStart = { 100, 100 };
     Vertex2 lineEnd = { 700, 500 };
 
     render_line(lineStart, lineEnd);
+    renderBuffer("line.bmp");
+    clear();
+
+    // Dibujar un polígono
+    Vertex2 polygonPoints[] = { {200, 200}, {300, 100}, {400, 200}, {300, 300} };
+    int numPoints = sizeof(polygonPoints) / sizeof(polygonPoints[0]);
+
+    render_polygon(polygonPoints, numPoints);
+    renderBuffer("polygon.bmp");
+    clear();
 
     return 0;
 }
 
 void render_point() {
-    // Colores
-    Color navyBlue(0, 0, 128);
-    Color white(255, 255, 255);
-
-    // Color de fondo y limpiar el buffer
-    setClearColor(navyBlue);
-    clear();
-
-    // Color de dibujo
-    setCurrentColor(white);
-
     // Dibujar un solo punto
     Vertex2 point1 = { 400, 300 };
     point(point1);
-
-    renderBuffer("single_point.bmp");
-    clear(); // Limpiar el buffer
 }
 
 void render_line(Vertex2 start, Vertex2 end) {
-    // Colores
-    Color navyBlue(0, 0, 128);
-    Color white(255, 255, 255);
+    int x0 = static_cast<int>(start.x);
+    int y0 = static_cast<int>(start.y);
+    int x1 = static_cast<int>(end.x);
+    int y1 = static_cast<int>(end.y);
 
-    // Color de fondo y limpiar el buffer
-    setClearColor(navyBlue);
-    clear();
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int sx = (x0 < x1) ? 1 : -1;
+    int sy = (y0 < y1) ? 1 : -1;
+    int err = dx - dy;
 
-    // Color de dibujo
-    setCurrentColor(white);
+    while (true) {
+        point({ static_cast<float>(x0), static_cast<float>(y0) });
 
-    int dx = end.x - start.x;
-    int dy = end.y - start.y;
-    int d = 2 * dy - dx;
-    int incrE = 2 * dy;
-    int incrNE = 2 * (dy - dx);
-    int x = start.x;
-    int y = start.y;
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
 
-    while (x < end.x) {
-        point({ x, y });
-        if (d <= 0) {
-            d += incrE;
-            x++;
-        } else {
-            d += incrNE;
-            x++;
-            y++;
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
         }
     }
+}
 
-    renderBuffer("line.bmp");
-    clear(); // Limpiar el buffer
+void render_polygon(Vertex2* points, int numPoints) {
+    for (int i = 0; i < numPoints; i++) {
+        Vertex2 startPoint = points[i];
+        Vertex2 endPoint = points[(i + 1) % numPoints];
+
+        render_line(startPoint, endPoint);
+    }
 }
