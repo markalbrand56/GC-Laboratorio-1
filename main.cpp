85a3360e38aaf862ca43ea5161b3a285a96968b7
Mark Albrand <mark.albrand5@gmail.com>
2023-07-07 19:18:56 -0600

    línea

--- main.cpp
+++ main.cpp
@@ -22,4 +22,19 @@ void render() {
     point(point1);
 
     renderBuffer("single_point.bmp");
+
+    // Dibujar una línea
+    Vertex2 lineStart = { 100, 100 };
+    Vertex2 lineEnd = { 700, 500 };
+
+    for (int i = 0; i < 100; ++i) {
+        Vertex2 linePoint = {
+            lineStart.x + (lineEnd.x - lineStart.x) * i / 100,
+            lineStart.y + (lineEnd.y - lineStart.y) * i / 100
+        };
+        point(linePoint);
+    }
+
+    renderBuffer("line.bmp");
+
 }
