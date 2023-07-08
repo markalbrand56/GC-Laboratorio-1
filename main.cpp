47a6baf30898887601958ef0d6675c8b488cd595
Mark Albrand <mark.albrand5@gmail.com>
2023-07-08 00:45:43 -0600

    línea

--- main.cpp
+++ main.cpp
@@ -1,20 +1,31 @@
 #include "framebuffer.h"
-#include "bmp_writer.h"
 
-void render();
+void render_point();
+void render_line(Vertex2 start, Vertex2 end);
 
 int main() {
-    render();
+    // Dibujar un solo punto
+    render_point();
+
+    // Dibujar una línea
+    Vertex2 lineStart = { 100, 100 };
+    Vertex2 lineEnd = { 700, 500 };
+
+    render_line(lineStart, lineEnd);
+
     return 0;
 }
 
-void render() {
+void render_point() {
+    // Colores
     Color navyBlue(0, 0, 128);
     Color white(255, 255, 255);
 
+    // Color de fondo y limpiar el buffer
     setClearColor(navyBlue);
     clear();
 
+    // Color de dibujo
     setCurrentColor(white);
 
     // Dibujar un solo punto
@@ -22,22 +33,41 @@ void render() {
     point(point1);
 
     renderBuffer("single_point.bmp");
+    clear(); // Limpiar el buffer
+}
 
-    // clear buffer
+void render_line(Vertex2 start, Vertex2 end) {
+    // Colores
+    Color navyBlue(0, 0, 128);
+    Color white(255, 255, 255);
+
+    // Color de fondo y limpiar el buffer
+    setClearColor(navyBlue);
     clear();
 
-    // Dibujar una línea
-    Vertex2 lineStart = { 100, 100 };
-    Vertex2 lineEnd = { 700, 500 };
+    // Color de dibujo
+    setCurrentColor(white);
 
-    for (int i = 0; i < 100; ++i) {
-        Vertex2 linePoint = {
-            lineStart.x + (lineEnd.x - lineStart.x) * i / 100,
-            lineStart.y + (lineEnd.y - lineStart.y) * i / 100
-        };
-        point(linePoint);
+    int dx = end.x - start.x;
+    int dy = end.y - start.y;
+    int d = 2 * dy - dx;
+    int incrE = 2 * dy;
+    int incrNE = 2 * (dy - dx);
+    int x = start.x;
+    int y = start.y;
+
+    while (x < end.x) {
+        point({ x, y });
+        if (d <= 0) {
+            d += incrE;
+            x++;
+        } else {
+            d += incrNE;
+            x++;
+            y++;
+        }
     }
 
     renderBuffer("line.bmp");
-
+    clear(); // Limpiar el buffer
 }
