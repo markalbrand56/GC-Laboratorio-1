6c66b72b7cb0e62aaeb6d08efe8dd274b64a4b26
Mark Albrand <mark.albrand5@gmail.com>
2023-07-11 23:15:55 -0600

    Fill

--- framebuffer.cpp
+++ framebuffer.cpp
@@ -31,3 +31,103 @@ void point(Vertex2 vertex) {
 void renderBuffer(const char *filename) {
     writeBMP(filename);
 }
+
+void render_point(int x, int y) {
+    // Dibujar un solo punto
+    Vertex2 point1 = { x, y };
+    point(point1);
+}
+
+void render_line(Vertex2 start, Vertex2 end) {
+    int x0 = static_cast<int>(start.x);
+    int y0 = static_cast<int>(start.y);
+    int x1 = static_cast<int>(end.x);
+    int y1 = static_cast<int>(end.y);
+
+    int dx = abs(x1 - x0);
+    int dy = abs(y1 - y0);
+    int sx = (x0 < x1) ? 1 : -1;
+    int sy = (y0 < y1) ? 1 : -1;
+    int err = dx - dy;
+
+    while (true) {
+        point({ x0, y0 });
+
+        if (x0 == x1 && y0 == y1) {
+            break;
+        }
+
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
+
+void drawPolygon(const Polygon& polygon) {
+    setCurrentColor(polygon.fillColor);
+
+    int numVertices = static_cast<int>(polygon.vertices.size());
+    if (numVertices < 3) {
+        return;
+    }
+
+    float yMin = SCREEN_HEIGHT;
+    float yMax = 0;
+
+    for (const Vertex2& vertex : polygon.vertices) {
+        if (vertex.y < yMin) {
+            yMin = vertex.y;
+        }
+        if (vertex.y > yMax) {
+            yMax = vertex.y;
+        }
+    }
+
+    for (int y = static_cast<int>(yMin); y <= yMax; y++) {
+        std::vector<float> intersections;
+
+        for (int i = 0; i < numVertices; i++) {
+            const Vertex2& currentVertex = polygon.vertices[i];
+            const Vertex2& nextVertex = polygon.vertices[(i + 1) % numVertices];
+
+            if ((currentVertex.y <= y && nextVertex.y > y) || (currentVertex.y > y && nextVertex.y <= y)) {
+                float intersectionX = currentVertex.x + (y - currentVertex.y) * (nextVertex.x - currentVertex.x) / (nextVertex.y - currentVertex.y);
+                intersections.push_back(intersectionX);
+            }
+        }
+
+        std::sort(intersections.begin(), intersections.end());
+
+        int numIntersections = static_cast<int>(intersections.size());
+        if (numIntersections < 2) {
+            continue;
+        }
+
+        for (int i = 0; i < numIntersections - 1; i += 2) {
+            int startX = static_cast<int>(intersections[i]);
+            int endX = static_cast<int>(intersections[i + 1]);
+
+            for (int x = startX; x < endX; x++) {
+                point({ (x), (y) });
+            }
+        }
+    }
+
+
+    setCurrentColor(polygon.borderColor);
+
+    for (int i = 0; i < numVertices - 1; i++) {
+        render_line(polygon.vertices[i], polygon.vertices[i + 1]);
+    }
+
+    render_line(polygon.vertices[numVertices - 1], polygon.vertices[0]);
+
+    renderBuffer("filled_polygon.bmp");
+    clear();
+}
--- main.cpp
+++ main.cpp
@@ -1,23 +1,21 @@
 #include "framebuffer.h"
 
-void render_point(int x, int y);
-void render_line(Vertex2 start, Vertex2 end);
-void render_polygon(Vertex2* points, int numPoints);
-
 int main() {
     // Colores
     Color navyBlue(0, 0, 128);
     Color white(255, 255, 255);
+    Color red(255, 0, 0);
+    Color black(0, 0, 0);
 
     // Color de fondo y limpiar el buffer
-    setClearColor(navyBlue);
+    setClearColor(white);
     clear();
 
     // Color de dibujo
-    setCurrentColor(white);
+    setCurrentColor(navyBlue);
 
     // Dibujar un solo punto
-    render_point(400,300);
+    render_point(400, 300);
     renderBuffer("point.bmp");
     clear();
 
@@ -29,59 +27,18 @@ int main() {
     renderBuffer("line.bmp");
     clear();
 
-    // Dibujar un polígono
+    // Dibujar un polígono con relleno y borde
     Vertex2 polygonPoints[] = { {200, 200}, {300, 100}, {400, 200}, {300, 300} };
     int numPoints = sizeof(polygonPoints) / sizeof(polygonPoints[0]);
 
-    render_polygon(polygonPoints, numPoints);
-    renderBuffer("polygon.bmp");
-    clear();
+    Polygon polygon;
+    for (int i = 0; i < numPoints; i++) {
+        polygon.vertices.push_back(polygonPoints[i]);
+    }
+    polygon.fillColor = red;
+    polygon.borderColor = black;
+
+    drawPolygon(polygon);
 
     return 0;
-}
-
-void render_point(int x, int y) {
-    // Dibujar un solo punto
-    Vertex2 point1 = { x, y };
-    point(point1);
-}
-
-void render_line(Vertex2 start, Vertex2 end) {
-    int x0 = static_cast<int>(start.x);
-    int y0 = static_cast<int>(start.y);
-    int x1 = static_cast<int>(end.x);
-    int y1 = static_cast<int>(end.y);
-
-    int dx = abs(x1 - x0);
-    int dy = abs(y1 - y0);
-    int sx = (x0 < x1) ? 1 : -1;
-    int sy = (y0 < y1) ? 1 : -1;
-    int err = dx - dy;
-
-    while (true) {
-        point({ static_cast<float>(x0), static_cast<float>(y0) });
-
-        if (x0 == x1 && y0 == y1) {
-            break;
-        }
-
-        int e2 = 2 * err;
-        if (e2 > -dy) {
-            err -= dy;
-            x0 += sx;
-        }
-        if (e2 < dx) {
-            err += dx;
-            y0 += sy;
-        }
-    }
-}
-
-void render_polygon(Vertex2* points, int numPoints) {
-    for (int i = 0; i < numPoints; i++) {
-        Vertex2 startPoint = points[i];
-        Vertex2 endPoint = points[(i + 1) % numPoints];
-
-        render_line(startPoint, endPoint);
-    }
 }
